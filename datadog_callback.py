ddc907a912f64c66bcbc388318ca0f863bbf19d1
Kristian Rune Larsen <krl@pyconsult.dk>
2017-05-18 15:43:53 +0200

    Added two environment variables for configuring datadog api key

--- datadog_callback.py
+++ datadog_callback.py
@@ -59,7 +59,8 @@ class CallbackModule(CallbackBase):
         else:
             print "Could not load configuration, invalid file: {}".format(file_path)
 
-        return conf_dict.get('api_key', ''), conf_dict.get('url', 'https://app.datadoghq.com')
+        return conf_dict.get('api_key', os.environ.get('DATADOG_API_KEY', '')), conf_dict.get('url', 'https://app.datadoghq.com')
+
 
     # Send event to Datadog
     def _send_event(self, title, alert_type=None, text=None, tags=None, host=None, event_type=None, event_object=None):
